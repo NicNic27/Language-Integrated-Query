dd5c52f571aa1bf1e7e5f314c2c8bc13e13f1138
nicTawingan <nictawingan@gmail.com>
2024-11-22 07:28:18 +0800

    LINQ Exercise code

---
+++ LINQ/LINQ/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQ
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Position: {Position}, Salary: {Salary:C}";
+        }
+    }
+
+}
